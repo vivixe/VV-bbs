9694123ded8e88b022af5b0c07be69ffe493b227
vivi <1104760234@qq.com>
2022-07-23 15:41:01 +0800

    最终完成

--- src/views/Home/MyHome.vue
+++ src/views/Home/MyHome.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: vivi.
  * @Date: 2022-07-22 15:48:08
- * @LastEditTime: 2022-07-22 21:58:17
+ * @LastEditTime: 2022-07-23 15:03:17
  * @FilePath: \demo-toutiao\src\views\Home\MyHome.vue
  * @Description:
 -->
 <template>
   <div class="home-container">
     <div v-if="navbarVisible">
-      <van-nav-bar title="程序狗" fixed >
+      <van-nav-bar title="程序狗" fixed>
         <template #right>
           <van-icon name="add-o" size="24" />
         </template>
@@ -26,14 +26,23 @@
         @cancel="onCancel"
       />
     </div>
-    <ArticleInfo
-      v-for="item in artlist"
-      :key="item.id"
-      :title="item.title"
-      :cmt-count="item.comm_count"
-      :time="item.pubdate"
-      :cover="item.cover"
-    ></ArticleInfo>
+    <van-pull-refresh v-model="refreshing" @refresh="onRefresh" :disabled="finished">
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <ArticleInfo
+          v-for="item in artlist"
+          :key="item.id"
+          :title="item.title"
+          :cmt-count="item.comm_count"
+          :time="item.pubdate"
+          :cover="item.cover"
+        ></ArticleInfo>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -49,14 +58,27 @@ export default {
       limit: 10,
       artlist: [],
       value: '',
-      navbarVisible: true
+      navbarVisible: true,
+      loading: true,
+      finished: false,
+      refreshing: false
     }
   },
   methods: {
     // 封装获取文章列表的方法
-    async initArticleList () {
+    async initArticleList (isRefresh) {
       const { data: res } = await getArticleListAPI(this.page, this.limit)
-      this.artlist = res
+      if (isRefresh) {
+        this.artlist = [...res, ...this.artlist]
+        this.refreshing = false
+      } else {
+        this.artlist = [...this.artlist, ...res]
+        this.loading = false
+      }
+
+      if (res.length === 0) {
+        this.finished = true
+      }
     },
     // 搜索按钮点击事件
     showSearch () {
@@ -69,6 +91,25 @@ export default {
     // 搜索框搜索按钮点击事件
     onSearch () {
       console.log(this.value)
+    },
+    onLoad () {
+      setTimeout(() => {
+        this.page++
+        this.initArticleList()
+      }, 1000)
+    },
+    onRefresh () {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          this.page++
+          this.initArticleList(true)
+          resolve()
+        }, 1000)
+      })
+      // this.page = 1
+      // this.artlist = []
+      // this.initArticleList()
+      // this.refreshing = false
     }
   },
   components: {
@@ -83,23 +124,23 @@ export default {
 <style lang="less" scoped>
 .home-container {
   padding: 46px 0 50px 0;
-  .home-search{
+  .home-search {
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
     z-index: 999;
-    .van-search{
+    .van-search {
       width: 100%;
-      .van-search__cancel{
+      .van-search__cancel {
         color: #333;
       }
     }
   }
   .van-nav-bar {
     background-color: #f9552f;
-    .van-icon{
-      color:#fff;
+    .van-icon {
+      color: #fff;
     }
   }
   /deep/.van-nav-bar__title {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+/*
+ * @Author: vivi.
+ * @Date: 2022-07-22 15:22:06
+ * @LastEditTime: 2022-07-23 15:35:56
+ * @FilePath: \demo-toutiao\vue.config.js
+ * @Description:
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: './'
+}
